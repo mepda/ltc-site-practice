3c73cc4eacfbb7fc775a7c844739544721fe96e3
Ben S <bsadick@gmail.com>
2018-06-09 11:21:51 +0900

    final commit for this class

---
+++ service-worker.js
@@ -0,0 +1,6 @@
+/* 
+TODO: 1.1 Import and check if workbox is available to use
+TODO: 1.2 Use a cache first approach for google fonts
+TODO: 1.3 Use a staleWhileRevalidate strategy for the static folder
+TODO: 1.4 Use a precacheAndRoute for index.html
+*/
